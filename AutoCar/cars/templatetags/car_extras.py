7ef161964d2fe57891c0b555b680a858b46a4188
emkheeey <marielkristineobsid.lorena@bicol-u.edu.ph>
2025-03-18 04:27:48 +0800

    comparison result frontend
    still need na ayusin amp.

--- AutoCar/cars/templatetags/car_extras.py
+++ AutoCar/cars/templatetags/car_extras.py
@@ -1,11 +1,16 @@
 from django import template
-
 register = template.Library()
 
 @register.filter
 def get_attr(obj, attr):
     """Get attribute from an object dynamically"""
-    return getattr(obj, attr, None)  # This uses Python's built-in getattr
+    try:
+        return getattr(obj, attr)
+    except (AttributeError, TypeError):
+        try:
+            return obj[attr]
+        except (KeyError, TypeError):
+            return None
 
 @register.filter
 def divided_by(value, arg):
