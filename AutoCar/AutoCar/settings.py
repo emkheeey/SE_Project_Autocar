0b35f03e84179eafe2ec9c27ec19aa67c2d10d71
Hope <161196465+CaffeinatedEnglishman@users.noreply.github.com>
2025-04-15 16:16:31 +0800

    Crossed the highest wall ever in my programming career
    deployed a clientside project 😎

--- AutoCar/AutoCar/settings.py
+++ AutoCar/AutoCar/settings.py
@@ -31,7 +31,8 @@ except Exception as e:
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 print(f"BASE_DIR: {BASE_DIR}")
-
+#nonsense I made it work
+#I AM AWESOOOOOME
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
